cb9825d9547784b660b14898024dd61c99e929d7
playX <gtashnik11@gmail.com>
2021-01-21 21:33:53 +0300

    No more line size feature

--- src/constants.rs
+++ src/constants.rs
@@ -1,12 +1,7 @@
 pub const BLOCK_SIZE: usize = 32 * 1024;
-#[cfg(feature = "line-size-128")]
+
 pub const LINE_SIZE: usize = 128;
-#[cfg(feature = "line-size-256")]
-pub const LINE_SIZE: usize = 256;
-#[cfg(feature = "line-size-512")]
-pub const LINE_SIZE: usize = 512;
-#[cfg(feature = "line-size-1024")]
-pub const LINE_SIZE: usize = 1024;
+
 pub const NUM_LINES_PER_BLOCK: usize = BLOCK_SIZE / LINE_SIZE;
 // Objects smaller than MEDIUM_OBJECT are allocated with the
 /// `NormalAllocator`, otherwise the `OverflowAllocator` is used.
